8690a5821a00fac3ecd11ab246a785e0c427fff9
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-04-03 16:12:05 +0900

    ch02. preprocess

--- common/util.py
+++ common/util.py
@@ -45,6 +45,23 @@ def clip_grads(grads, max_norm):
     total_norm = np.sqrt(total_norm)
 
     rate = max_norm / (total_norm + 1e-6)
-    if rage < 1:
+    if rate < 1:
         for grad in grads:
             grad *= rate
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+    
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
--- common/util_test.py
+++ common/util_test.py
@@ -1,21 +1,35 @@
+import unittest
+
 import sys, os
 sys.path.append(os.pardir)
-import numpy as np
-from util import im2col, col2im
+from common.np import *
+from common.util import im2col, col2im, clip_grads, preprocess
 
-def im2col_test():
-    x = np.random.randn(5, 1, 28, 28)
-    col = im2col(x, 4, 4, stride=1, pad=0)
+class UtilTest(unittest.TestCase):
+    def test_im2col_transforms(self):
+        x = np.random.randn(5, 1, 28, 28)
+        col = im2col(x, 4, 4, stride=1, pad=0)
 
-    print(x.shape)
-    print(col.shape)
+        self.assertSequenceEqual((3125, 16), col.shape)
 
-def col2im_test():
-    col = np.random.randn(3125, 16)
-    x = col2im(col, (5, 1, 28, 28), 4, 4, stride=1, pad=0)
+    def test_col2im_transforms(self):
+        col = np.random.randn(3125, 16)
+        x = col2im(col, (5, 1, 28, 28), 4, 4, stride=1, pad=0)
 
-    print(col.shape)
-    print(x.shape)
+        self.assertSequenceEqual((5, 1, 28, 28), x.shape)
 
-im2col_test()
-col2im_test()
+    def test_preprocess_returns_corpus(self):
+        text = 'you say goodbye and I say hello.'
+        corpus, word_to_id, id_to_word = preprocess(text)
+
+        ex_corpus = np.array([0,1,2,3,4,1,5,6])
+        ex_w2id = {'you': 0, 'say': 1, 'goodbye': 2, 'and': 3, 'i': 4, 'hello': 5, '.': 6}
+        ex_id2w = {0: 'you', 1: 'say', 2: 'goodbye', 3: 'and', 4: 'i', 5: 'hello', 6: '.'}
+
+        self.assertTrue((ex_corpus == corpus).all())
+        self.assertDictEqual(ex_w2id, word_to_id)
+        self.assertDictEqual(ex_id2w, id_to_word)
+
+
+if __name__ == '__main__':
+    unittest.main()
