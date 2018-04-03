2c916c45af03afd231c9626b632a2c1112301221
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-04-03 12:36:57 +0900

    trainer

--- common/util.py
+++ common/util.py
@@ -35,3 +35,14 @@ def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
             img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
     
     return img[:, :, pad:H + pad, pad:W + pad]
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rage < 1:
+        for grad in grads:
+            grad *= rate
