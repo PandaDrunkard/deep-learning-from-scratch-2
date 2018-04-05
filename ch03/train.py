d155230bb1c1a67e1dd134a5395eb6860d4cd2b4
PandaDrunkard <tosho.hirasawa@gmail.com>
2018-04-06 01:49:34 +0900

    simple cbow

---
+++ ch03/train.py
@@ -0,0 +1,37 @@
+import sys, os
+sys.path.append(os.pardir)
+from common.trainer import Trainer
+from common.optimizer import Adam
+from common.util import preprocess, create_contexts_target, convert_one_hot
+
+from simple_cbow import SimpleCBOW
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+window_size = 1
+hidden_size = 5
+batch_size = 3
+max_epoch = 1000
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+
+vocab_size = len(word_to_id)
+contexts, target = create_contexts_target(corpus, window_size=window_size)
+target = convert_one_hot(target, vocab_size)
+
+print(contexts)
+contexts = convert_one_hot(contexts, vocab_size)
+print(contexts)
+
+model = SimpleCBOW(vocab_size, hidden_size)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+trainer.fit(contexts, target, max_epoch, batch_size)
+trainer.plot()
+
+word_vecs = model.word_vecs
+for word_id, word in id_to_word.items():
+    print(word, word_vecs[word_id])
