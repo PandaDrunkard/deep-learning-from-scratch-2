f22f4f4ed5672b948be4c9398db90e9b20d0b0f6
Tosho Hirasawa <tosho@tosho-lab.local>
2018-04-09 23:12:12 +0900

    unigram sampler

--- common/util.py
+++ common/util.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.append(os.pardir)
 from common.np import *
+import collections
 
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     """
@@ -168,3 +169,41 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
     
     return np.array(contexts), np.array(target)
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+        
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+        
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+    
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= p.sum()
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+        else:
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                                                   replace=True, p=self.word_p)
+            
+        return negative_sample
